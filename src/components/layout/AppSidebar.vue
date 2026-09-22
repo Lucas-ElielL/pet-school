fdd538f29001b241636c4b63fec5925556d3642f
Lucas-ElielL <lucaseliel15032010@gmail.com>
2026-09-22 09:13:55 -0300

    COmeço do HTML da atividade do professor

---
+++ src/components/layout/AppSidebar.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { RouterLink } from "vue-router";
+</script>
+<template>
+  <nav>
+    <div class="flex-column nav-pills gap-1">
+      <Router-link to="/Pets">
+        <i class="fa-solid fa-fire"></i>
+        Pets
+      </Router-link>
+
+      <Router-link to="/pets/novo">
+        <i class="fa-solid fa-plus"></i>
+        Novo Pet
+      </Router-link>
+
+    </div>
+  </nav>
+</template>
